e8159f047df49e40cf2370944c0400fed7a8dcf2
Suvadip Mukherjee <suvadip21@persephone.laiq.pasteur.fr>
2019-01-03 14:59:57 +0100

    Added GAC code

--- segmentation/levelset/segmentation_filters.py
+++ segmentation/levelset/segmentation_filters.py
@@ -1,10 +1,9 @@
 import numpy as np
 import scipy.ndimage as nd
-# import matplotlib.pyplot as plt
 from misc.helpers import StdIO as IO
 from matplotlib import pyplot as plt
-
 import levelset_helper as H
+from filters.spatial import Filter
 
 eps = np.finfo(float).eps
 
@@ -22,7 +21,16 @@ class LevelSetFilter:
         self.max_iter = max_iters
         self.tolerance = convg_error
 
+    """
+    Active contour without edges - Chan & Vese
+    """
     def chan_vese(self, mu=0.1, color='r', disp_interval=20):
+        """
+        :param mu: Smoothness parameteer
+        :param color: contour color
+        :param disp_interval: contour display interval
+        :return: levelset, # iterations
+        """
         phi = H.mask2phi(self.init_mask)
         phi0 = np.copy(phi)
         u = self.img
@@ -52,6 +60,7 @@ class LevelSetFilter:
             phi = H.NeumannBoundCond(phi)
             phi = H.sussman(phi, 0.5)
 
+            # -- Convergence criteria
             new_mask = 1.*(phi >=0)
             c = H.convergence(prev_mask, new_mask, self.tolerance, c)
             if c <= 5:
@@ -60,6 +69,7 @@ class LevelSetFilter:
             else:
                 stop = True
 
+            # -- Display of curve
             if (disp_interval > 0 and np.mod(its, disp_interval)==0):
                 ax.cla()
                 ax.imshow(u, cmap='gray')
@@ -70,13 +80,84 @@ class LevelSetFilter:
 
         return phi, its
 
+    """
+    Geodesic Active Contours - Sapiro et al.
+    """
+    def gac(self, mu=1., c0=-0.8, sigma=2., color='b', disp_interval=20):
+        """
+        :param mu: mu=0 implies mean curvature motion
+        :param c0: advection term
+        :param sigma: std. deviation of gaussian for the edge feature map
+        :param color: contour display color
+        :param disp_interval: contour diaplay interval. Not shown if negative
+        :return: levelset, # iterations
+        """
+        phi = H.mask2phi(self.init_mask)
+        phi0 = np.copy(phi)
+        u = self.img
+        stop = False
+        prev_mask = self.init_mask
+        its = 0
+        c = 0
+
+        if (disp_interval > 0):
+            fig = plt.figure()
+            ax = fig.add_subplot(1,1,1)
+
+        # -- Compute the image derived feature map
+        _, grad_sigma_u = Filter.gradient_filter(u, sigma_mm=sigma)
+        g = 1./(0.01 + grad_sigma_u**(1.0))       # edge feature map
+        g = (g - g.min())/(g.max() - g.min())
+        grad_g = np.gradient(g)
+
+        while (its < self.max_iter and stop==False):
+            grad_phi = np.gradient(phi)
+            grad_phi_mag = np.sqrt(grad_phi[0]**2 + grad_phi[1]**2)
+            kappa = H.curvature_central(phi)
+
+            dphi_dt1 = g * grad_phi_mag                                     # Advection term
+            dphi_dt2 = g * grad_phi_mag * kappa;                            # Curvature term
+            dphi_dt3 = grad_g[0] * grad_phi[0]+ grad_g[1] * grad_phi[1]     # Edge attraction term
+
+            # dphi_dt1 = dphi_dt1 / (eps + np.max(np.abs(dphi_dt1)))
+            # dphi_dt2 = dphi_dt2 / (eps + np.max(np.abs(dphi_dt2)))
+            # dphi_dt3 = dphi_dt3 / (eps + np.max(np.abs(dphi_dt3)))
+
+            F = c0 * dphi_dt1 + mu * dphi_dt2 + dphi_dt3
+            dt = 0.6/(np.max(np.abs(F)) + eps)                              # CFL criteria
+
+            phi += dt * F
+            phi = H.NeumannBoundCond(phi)
+            phi = H.sussman(phi, 0.5)
+
+            #-- Convergence criteria
+            new_mask = 1.*(phi >=0)
+            c = H.convergence(prev_mask, new_mask, self.tolerance, c)
+            if c <= 5:
+                its = its + 1
+                prev_mask = new_mask
+            else:
+                stop = True
+
+            # -- Display of curve
+            if (disp_interval > 0 and np.mod(its, disp_interval)==0):
+                ax.cla()
+                ax.imshow(u, cmap='gray')
+                ax.contour(phi, levels=[0], colors=color)
+                ax.contour(phi0, levels=[0], colors='g')
+                ax.set_axis_off()
+                plt.draw()
+                plt.pause(1e-5)
+
+        return phi, its
+
 if __name__=='__main__':
     from misc.helpers import StdIO as IO
     img = IO.imread_2d('../../image_2.png')
     mask = np.zeros(img.shape)
-    mask[20:100, 40:120] = 1.
-    seg, its = LevelSetFilter(img, init_mask=mask, max_iters=1000, convg_error=0.5).chan_vese(mu=1.0, color='y', disp_interval=50)
-
+    mask[40:80, 40:100] = 1.
+    # seg, its = LevelSetFilter(img, init_mask=mask, max_iters=1000, convg_error=0.5).chan_vese(mu=1.0, color='y', disp_interval=50)
+    seg, its = LevelSetFilter(img, init_mask=mask, max_iters=1500, convg_error=0.1).gac(mu=1.0, c0=-1.5, sigma=2.0, color='b', disp_interval=50)
     IO.imoverlay(img, seg, title='Final result', linewidth=4)
 
 
